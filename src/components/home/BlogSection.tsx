ca1c3996f264fce36a18fb844743cdcd60785d91
kcrypto126 <technt22@gmail.com>
2025-07-02 01:20:09 +0900

    feat: add blog section

--- src/components/home/BlogSection.tsx
+++ src/components/home/BlogSection.tsx
@@ -3,11 +3,11 @@
 import Container from "../layouts/Container";
 
 const BlogSection = () => {
-    return (
-        <Container className="py-8 md:py-25">
-            <div className="flex flex-col gap-6">
-                sdf
-            </div>
-        </Container>
-    )
-}
+  return (
+    <Container className="py-8 md:py-25">
+      <div className="flex flex-col gap-6">sdf</div>
+    </Container>
+  );
+};
+
+export default BlogSection;
